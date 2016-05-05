4742ecfc8ea2751c7cd515e00cb5ca8b672e4d82
flagg41 <sneer41@gmail.com>
2016-05-05 10:18:14 +0200

    start of algorithm
    as above

--- kmeans.py
+++ kmeans.py
@@ -1,18 +1,21 @@
+# k-means algorithm on square [0,1] x [0,1]
+
 import numpy as np
+import sets
 
-sample_size = 1000
-
-def closest_point(points, point):
+def closest_point(point, points):
     dist = np.sum((points - point)**2, axis = 1)
     return np.argmin(dist)
 
-point = np.asarray([1,2,3,4])
+# hard-coded data
+data_size = 100
+k = 4
+data_indices = range(data_size)
 
-points = np.asarray([[1,2,3,0],[0,2,3,4],[1,2,3,4]])
+centroids = np.random.rand(k, 2)
+data = np.random.rand(data_size, 2)
+cluster_assignment = dict(zip(data_indices, [0] * data_size))
 
-print closest_point(points, point)
-print points[:, 0]
-
-#added comment
-
-#another test
+while True:
+    for i in data_indices:
+        cluster_assignment[i] = closest_point(data[:, i], centroids)    
