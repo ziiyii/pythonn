93449d455921b482999fb59022f1ba18eabb322e
flagg41 <sneer41@gmail.com>
2016-05-06 01:33:11 +0200

    slicing_indices ok, ale dalej nei wiem

--- kmeans.py
+++ kmeans.py
@@ -1,48 +1,61 @@
 # k-means algorithm on square [0,1] x [0,1]
 
 import numpy as np
-import sets
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
 
 def closest_point(point, points):
     dist = np.sum((points - point)**2, axis = 1)
     return np.argmin(dist)
 
+
 def slicedict(dict, index):
     return {k: v for k, v in dict.iteritems() if v == index}
 
+
 # hard-coded data
-data_size = 10
-k = 3
+data_size = 20
+k = 4
 centroids_indices = range(k)
 data_indices = range(data_size)
 
+
 # algorithm beginning
 centroids = np.random.rand(k, 2)
-new_centroids = np.zeros((k, 2))
+old_centroids = np.zeros((k, 2))
 data = np.random.rand(data_size, 2)
 cluster_assignment = dict(zip(data_indices, [0] * data_size))
 
+
 while True:
-    print centroids, new_centroids
-    raw_input()
     for i in data_indices:
+        b = cluster_assignment[i]
         cluster_assignment[i] = closest_point(data[i, :], centroids)
+        c = cluster_assignment[i]
+        # if(b - c != 0):
+        #     print b, c
 
     for i in centroids_indices:
-        slicing_indexes = slicedict(cluster_assignment, i).keys()
-        if(np.array_equal(slicing_indexes, [])):
-            new_centroids[i] = centroids[i]
-        else:
-            new_centroids[i] = np.mean(data[slicing_indexes], axis = 0)
-        print centroids, new_centroids
-        raw_input()
+        slicing_indices = slicedict(cluster_assignment, i).keys()
+        print i, slicing_indices, '\n'
+        a = centroids[i]
+        if not(np.array_equal(slicing_indices, [])):
+            centroids[i] = np.mean(data[slicing_indices], axis = 0)
+        b = centroids[i]
+        if not (np.array_equal(a,b)):
+            print b, c
 
-
-    if(np.allclose(centroids, new_centroids, rtol = 0, atol = 0)):
+    if np.allclose(centroids, old_centroids, rtol = 0, atol = 0):
         break
-    else:
-        centroids = new_centroids[:]
+
+    old_centroids = centroids[:]
+
 
     print "hehe"
 
+# ind = [0,0,1,1,2,2,2,2,2,2]
+#
+# plot_clusters(data,ind)
 print "wyszedlem z petli"
+
