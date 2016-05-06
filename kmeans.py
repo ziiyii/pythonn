94c0647c416590ddf43e34cd2c3790dc05d49b75
flagg41 <sneer41@gmail.com>
2016-05-06 20:30:12 +0200

    version with unnecessary dictionary
    as above

--- kmeans.py
+++ kmeans.py
@@ -1,12 +1,13 @@
 # k-means algorithm on square [0,1] x [0,1]
 
+import copy
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+import matplotlib.cm as cm
 
 
 def closest_point(point, points):
-    dist = np.sum((points - point)**2, axis = 1)
+    dist =  np.sum((points - point)**2, axis = 1)
     return np.argmin(dist)
 
 
@@ -14,11 +15,22 @@ def slicedict(dict, index):
     return {k: v for k, v in dict.iteritems() if v == index}
 
 
+def plot_iteration(data, cluster_assignment, centroids):
+    for index, col in zip(centroids_indices, colors):
+        slicing_indices = slicedict(cluster_assignment, index).keys()
+        if not (np.array_equal(slicing_indices, [])):
+            x = data[slicing_indices][:, 0]
+            y = data[slicing_indices][:, 1]
+            plt.scatter(x, y, color = col, s = [50]*len(x))
+        plt.scatter(centroids[index, 0], centroids[index, 1], color = col, s = [200], edgecolors = 'black')
+
+
 # hard-coded data
-data_size = 20
-k = 4
+data_size = 10000
+k = 16
 centroids_indices = range(k)
 data_indices = range(data_size)
+colors = cm.Dark2(np.linspace(0, 1, k))
 
 
 # algorithm beginning
@@ -30,32 +42,19 @@ cluster_assignment = dict(zip(data_indices, [0] * data_size))
 
 while True:
     for i in data_indices:
-        b = cluster_assignment[i]
         cluster_assignment[i] = closest_point(data[i, :], centroids)
-        c = cluster_assignment[i]
-        # if(b - c != 0):
-        #     print b, c
 
     for i in centroids_indices:
         slicing_indices = slicedict(cluster_assignment, i).keys()
-        print i, slicing_indices, '\n'
-        a = centroids[i]
         if not(np.array_equal(slicing_indices, [])):
             centroids[i] = np.mean(data[slicing_indices], axis = 0)
-        b = centroids[i]
-        if not (np.array_equal(a,b)):
-            print b, c
 
     if np.allclose(centroids, old_centroids, rtol = 0, atol = 0):
         break
 
-    old_centroids = centroids[:]
+    old_centroids = copy.copy(centroids)
 
-
-    print "hehe"
-
-# ind = [0,0,1,1,2,2,2,2,2,2]
-#
-# plot_clusters(data,ind)
-print "wyszedlem z petli"
-
+    plot_iteration(data, cluster_assignment, centroids)
+    plt.draw()
+    plt.pause(0.1)
+    plt.clf()
