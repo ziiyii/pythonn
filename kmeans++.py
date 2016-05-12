23ff535a1085324a989e5a25c209885ec2167063
flagg41 <sneer41@gmail.com>
2016-05-12 20:15:13 +0200

    small visual improvements
    size of points changed, more OO version

--- kmeans++.py
+++ kmeans++.py
@@ -22,24 +22,32 @@ def plot_iteration(data, cluster_assignment, centroids):
         if not (np.array_equal(slicing_indices, [])):
             x = data[slicing_indices][:, 0]
             y = data[slicing_indices][:, 1]
-            plt.scatter(x, y, color = col, s = [50]*len(x))
+            plt.scatter(x, y, color = col, s = [point_size]*len(x))
         plt.scatter(centroids[index, 0], centroids[index, 1], color = col,
-                    s = [100], edgecolors = 'black')
+                    s = [centroid_size], edgecolors = 'black')
     plt.draw()
     plt.pause(waiting_time)
     plt.clf()
+    return 0
 
 
-def plot_data(data, centroids):
+def animate_centroids_arrangement(data, centroids):
     x = data[:, 0]
     y = data[:, 1]
-    plt.scatter(x, y, color='black', s=[50] * len(x))
-    x = centroids[:, 0]
-    y = centroids[:, 1]
-    plt.scatter(x, y, color='green', s=[100] * len(x))
+    plt.scatter(x, y, color='black', s=[point_size] * len(x))
     plt.draw()
     plt.pause(waiting_time)
     plt.clf()
+    for c in range(1, len(centroids)):
+        x = data[:, 0]
+        y = data[:, 1]
+        plt.scatter(x, y, color='black', s=[point_size] * len(x))
+        x = centroids[:c, 0]
+        y = centroids[:c, 1]
+        plt.scatter(x, y, color='red', s=[centroid_size] * len(x))
+        plt.draw()
+        plt.pause(waiting_time)
+        plt.clf()
 
 
 def generate_spheric_data(data_size, k):
@@ -47,14 +55,11 @@ def generate_spheric_data(data_size, k):
     weights = weights / np.sum(weights)
     number_in_clusters = np.floor(np.multiply(weights, data_size))
     new_data_size = int(np.sum(number_in_clusters))
-
-    means1 = np.random.choice(40, k, replace = False)
-    means2 = np.random.choice(40, k, replace = False)
+    means1 = np.random.choice(20, k, replace = False)
+    means2 = np.random.choice(20, k, replace = False)
     variances = np.random.randint(1, 2, k)
-
     data = np.empty([new_data_size, 2])
     index_begin = 0
-
     for i in range(k):
         size = number_in_clusters[i]
         index_end = index_begin + size
@@ -63,7 +68,6 @@ def generate_spheric_data(data_size, k):
         data[index_begin:index_end, 1] = np.random.normal(means2[i],
                                                             variances[i], size)
         index_begin = index_end
-
     return data
 
 
@@ -72,9 +76,7 @@ def centroids_arrangement(data, k):
     data_indices = range(data_size)
     centroids = np.empty([k, 2])
     centroids[0, :] = data[np.random.choice(data_size, 1), :]
-
     for c in range(1, k):
-        plot_data(data, centroids[:c, :])
         distr = [0] * data_size
         for i in data_indices:
             index = closest_point(data[i, :], centroids[:c, :])
@@ -85,47 +87,52 @@ def centroids_arrangement(data, k):
                             :]
     return centroids
 
-def compute_and_plot(data, centroids):
-    k = len(centroids)
-    centroids_indices = range(k)
-    old_centroids = np.zeros((k, 2))
+
+def assign_points(data, centroids):
     data_size = len(data)
     data_indices = range(data_size)
     cluster_assignment = [0] * data_size
+    for i in data_indices:
+        cluster_assignment[i] = closest_point(data[i, :], centroids)
+    return cluster_assignment
 
+
+def calculate_centroids(data, centroids, cluster_assignment):
+    k = len(centroids)
+    centroids_indices = range(k)
+    for i in centroids_indices:
+        slicing_indices = slice_list(cluster_assignment, i)
+        if not (np.array_equal(slicing_indices, [])):
+            centroids[i] = np.mean(data[slicing_indices], axis=0)
+    return centroids
+
+
+def animate_algorithm(data, centroids):
+    old_centroids = np.zeros((k, 2))
     while True:
-        for i in data_indices:
-            cluster_assignment[i] = closest_point(data[i, :], centroids)
-
+        cluster_assignment = assign_points(data, centroids)
         plot_iteration(data, cluster_assignment, centroids)
-
-        for i in centroids_indices:
-            slicing_indices = slice_list(cluster_assignment, i)
-            if not(np.array_equal(slicing_indices, [])):
-                centroids[i] = np.mean(data[slicing_indices], axis = 0)
-
+        centroids = calculate_centroids(data, centroids, cluster_assignment)
         plot_iteration(data, cluster_assignment, centroids)
-
         if np.allclose(centroids, old_centroids, rtol = 0, atol = 0):
             break
-
         old_centroids = copy.copy(centroids)
-
     return 0
 
 
 # hard-coded data
-data_size = 1000
-k = 7
+data_size = 2000
+k = 8
+point_size = 5
+centroid_size = 60
 waiting_time = 0.0001
-# np.random.seed(2)
+#np.random.seed(99)
+
 data = generate_spheric_data(data_size, k)
 # size of data changed a little bit, but order of magnitude remained the same
 
 centroids = centroids_arrangement(data, k)
 
-plot_data(data, centroids)
+animate_centroids_arrangement(data, centroids)
 
-compute_and_plot(data, centroids)
-
-#now everything is nice OO and better distr
+animate_algorithm(data, centroids)
